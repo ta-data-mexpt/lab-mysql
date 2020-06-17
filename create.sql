8967b41cd9333d54452438773babb55cf3dd9e88
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-06-17 15:25:34 -0500

    Update create

--- create.sql
+++ create.sql
@@ -8,7 +8,7 @@ USE lab_mysql;
 
 CREATE TABLE cars (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
-    vin VARCHAR(20),
+    vin VARCHAR(20) NOT NULL,
     manufacturer VARCHAR(20),
     model VARCHAR(20),
     year CHAR(4),
@@ -18,7 +18,7 @@ SHOW COLUMNS FROM cars;
 
 CREATE TABLE customers(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
-    cus_id CHAR(5),
+    cus_id CHAR(5) NOT NULL,
     name VARCHAR(50),
     phone VARCHAR(20),
     email VARCHAR(20),
@@ -27,13 +27,12 @@ CREATE TABLE customers(
     state_province VARCHAR(20),
     country VARCHAR(40),
     postal CHAR(5)
-
 );
 SHOW COLUMNS FROM customers;
 
 CREATE TABLE salespersons(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
-    staff_id CHAR(5),
+    staff_id CHAR(5) NOT NULL,
     name VARCHAR(50),
     store VARCHAR(40)
 );
@@ -41,8 +40,8 @@ SHOW COLUMNS FROM salespersons;
 
 CREATE TABLE invoices(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
-    invoice_number CHAR(5),
-    date TIMESTAMP,
+    invoice_number CHAR(10) NOT NULL,
+    date DATE,
     car INT UNSIGNED NOT NULL,
     customer INT UNSIGNED NOT NULL,
     sales_person INT UNSIGNED NOT NULL
