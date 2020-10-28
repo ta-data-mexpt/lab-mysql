474cba9e87997097002f700312c894d80ef79dae
garcia808 <garcia.ga@outlook.com>
2020-10-27 18:41:34 -0600

    añadiendo archivo update

---
+++ update.sql
@@ -0,0 +1,13 @@
+USE lab_mysql;
+UPDATE salespersons
+SET store='Miami'
+WHERE ID=5;
+UPDATE customers
+SET email='ppicasso@gmail.com'
+WHERE ID=1;
+UPDATE customers
+SET email='lincoln@us.gov'
+WHERE ID=2;
+UPDATE customers
+SET email='hello@napoleon.me'
+WHERE ID=3;
