1ab0b99362cb8dddb9ca8bb5481557589b0eb163
urdaibayc <urdaibayc@gmail.com>
2020-10-30 15:16:06 -0600

    Corrected & improved.

--- create.sql
+++ create.sql
@@ -1,16 +1,39 @@
-USE lab-mysql;
+USE lab_mysql;
 
-create table customers(Customer_ID CHAR(20) not null primary key, FirstName CHAR(20) NOT NULL, LastName char(29) not null, Phone char(20) not null, Email char(20) not null, Address char not null, City char(20) not null, State char(20) not null, Zipcode char(10) not null, Country char(7) default "Mx");
+create table customers(
+	ID int not null primary key AUTO_INCREMENT,
+	Customer_ID CHAR(20) not null,
+	FirstName CHAR(20) NOT NULL,
+	LastName char(29) not null,
+	Phone char(20) not null, Email char(20) not null,
+	Address char not null,
+	City char(20) not null,
+	State char(20) not null,
+	Zipcode char(10) not null,
+	Country char(7) default "Mx");
 
-create table staff (Employee_ID int not null primary key, FirstName char(20) not null, LastName char(20) not null, Store char(20) not null);
+create table staff (
+	ID int not null primary key AUTO_INCREMENT,
+	Employee_ID int not null,
+	FirstName char(20) not null,
+	LastName char(20) not null,
+	Store char(20) not null);
 
-create table cars(vin char(50) not null primary key, Manufacturer char(20) not null, Model char(20) not null, Year year not null, Color char(10) not null);
+create table cars(
+	ID int not null AUTO_INCREMENT primary key,
+	vin char(50) not null,
+	Manufacturer char(20) not null,
+	Model char(20) not null,
+	Year year not null,
+	Color char(10) not null);
 
-create table invoices (Invoice_ID int not null primary key, Date timestamp not null, Car char, Customer char, Employee int);
+create table invoices (
+	ID int not null primary key AUTO_INCREMENT,
+	Invoice_ID int not null,
+	Date timestamp not null,
+	Car int not null, foreign key (Car) references cars(ID),
+	Customer int not null, foreign key (Customer) references customers(ID),
+	Employee int not null, foreign key (Employee) references staff(ID));
 
-alter table invoices add foreign key (Car) references cars(vin);
-alter table invoices add foreign key (Employee) references staff(Employee_ID);
-ALTER TABLE invoices MODIFY COLUMN Car char not null;
-ALTER TABLE invoices MODIFY COLUMN Customer char not null;
-ALTER TABLE invoices MODIFY COLUMN Employee int not null;
+
 
