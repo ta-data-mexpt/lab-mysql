e7d5a244f718904286a637a3b51672b510289f57
MenaGarcJ <menagarcj0@gmail.com>
2021-05-07 20:20:27 -0500

    Challenge 3

--- seeding.sql
+++ seeding.sql
@@ -1,1 +1,5 @@
-SELECT * FROM Invoices; 
+
+INSERT INTO Customers
+	VALUES (005, 'Maluma', 'Beibi', 32165478, 'papi.juancho@theco.io', 'Calle 1, No. 002, Col. Parce', 'Cali', 'Cali', 'Colombia', 75395128);
+    
+SELECT * FROM Customers;
