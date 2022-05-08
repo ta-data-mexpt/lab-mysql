0c8a90fd0b3765746b6e6354f8345127e9770fbb
Dennis Rodriguez <dennis.rodriguez1094@gmail.com>
2022-05-08 15:57:14 -0500

    final

---
+++ seeding.sql
@@ -0,0 +1,27 @@
+INSERT INTO Cars VALUES(1,"350KGL", "WV", "2013", "NEGRO"),
+    -> (2, "870JJF", "FORD", "2000", "ROJO"),
+    -> (3, "264UUT", "BMW", "2018", "NEGRO"),
+    -> (4, "328PKH", "FORD", "2003", "GRIS"),
+    -> (5, "93UGHE", "NISSAN", "2007", "AZUL");
+
+INSERT INTO Customers VALUES(1, "Juan", "5523894578", "juan@cars.com", "Fuentes brotantes 26", "CDMX", "CDMX", "Mexico", 03211),
+(2, "Luis", "5598645231", "luis@cars.com", "Manzanillo 89", "Guadalajara", "Jalisco", "Mexico", 09645),
+(3, "Ana", "5536794105", "ana@cars.com", "Cascada 71", "CDMX", "CDMX", "Mexico", 07942),
+(4, "Luisa", "5541367520", "luisa@cars.com", "Piedra Lisa 56", "Colima", "Colima", "Mexico", 04623),
+(5, "Mariana", "5530024896", "mariana@cars.com", "Bradley 56", "CDMX", "CDMX", "Mexico", 01570);
+
+
+INSERT INTO Salespersons VALUES(1, "Alexa", "CentroCDMX"), 
+(2, "ALejandro", "CentroGDL"),
+(3, "Carlos", "CentroGDL"),
+(4, "Berenice", "SurCDMX"),
+(5, "Diego", "CentroCDMX"),
+(6, "Sara", "SurCDMX");
+
+INSERT INTO Invoices VALUES(568912341, "20210210", 1, 2, 2), 
+(568912342, "20210510", 4,3,6 ),
+(568912343, "20210810", 2,5,3 ),
+(568912344, "20211110", 5,2,2 ),
+(568912345, "20211015", 1,4,4),
+(568912346, "20211022", 3,1,5);
+
